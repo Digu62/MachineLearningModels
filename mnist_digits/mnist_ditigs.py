00270585bbabe29c5647d06c3d063acb1ba89105
Digu62 <rodrigomatospeixoto@gmail.com>
2023-02-20 22:46:51 -0300

    Updates in mnist model

--- mnist_digits/mnist_ditigs.py
+++ mnist_digits/mnist_ditigs.py
@@ -6,7 +6,7 @@ from random import randint
 import matplotlib.pyplot as plt
 
 from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Input, Dropout, Dense, Flatten
+from tensorflow.keras.layers import Input, Dropout, Dense, Flatten, Conv2D, MaxPooling2D
 
 from sklearn.model_selection import train_test_split
 
@@ -42,14 +42,54 @@ x_train = data.drop(['label'],axis=1)
 y_train = data['label']
 print(f"Shape:\n {x_train.shape}")
 
+#Converte o array para o formato de imagem
+x_train = x_train.values.reshape((x_train.shape[0],28,28,1))
+
+#Normaliza os dados
+x_train = x_train/255
+
 #Resahping to plot images
-resheaped = np.array(x_train).reshape(-1,28,28)
-print(f"Reshaped:\n {resheaped.shape}")
+# resheaped = np.array(x_train).reshape(-1,28,28)
+print(f"Reshaped:\n {x_train.shape}")
 plt.figure(figsize=(7,7))
 for i in range(9):
     plt.subplot(3,3, i+1)
     image = randint(0,len(x_train))
-    plt.imshow(resheaped[image],cmap='gray',interpolation='none')
+    plt.imshow(x_train[image],cmap='gray',interpolation='none')
     plt.title(f"Label {y_train[image]}")
     plt.axis('off')
 plt.show()
+
+x_train, x_val, y_train, y_val = train_test_split(x_train,y_train, test_size=0.2, random_state=42)
+
+
+print(f"Train shape: {x_train.shape}")
+print(f"Train y shape: {y_train.shape}")
+print(f"Val shape: {x_val.shape}")
+print(f"Val y shape: {y_val.shape}")
+
+#Creating model
+model = Sequential(name="Mnist_digits")
+model.add(Input(shape=(28,28,1), name="input_layer"))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D((2,2)))
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation='relu'))
+model.add(MaxPooling2D((2,2,)))
+model.add(Flatten())
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+batch_size = 64
+epochs = 50
+
+history = model.fit(x=x_train,
+                    y=y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_val,y_val))
+
+
