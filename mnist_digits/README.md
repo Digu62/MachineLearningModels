fc97f3806c3e10a76ef5187f2969fafb194ceda3
Digu62 <rodrigomatospeixoto@gmail.com>
2023-02-23 22:15:52 -0300

    Updates in mnist model

---
+++ mnist_digits/README.md
@@ -0,0 +1,19 @@
+# Contextualization
+> The Mnist Digits dataset is a challenge when we have a collection of images with handmaded numbers from 0 to 9. 
+
+# Challenge
+> The challenge consists in create a machine learning model to predict wich is the number in each imagen provided.
+
+# Data
+> The data containt 785 columns, where the first columns has the label and the other 784 columns the pixel values from the image. So, each sample is a image 28x28 pixels in gray scale (0 to 255).
+
+## Tasks
+- [ ] Implement folder creation in script code
+- [x] Implement machine learning algorithm
+- [ ] Train and test model
+- [ ] Save results
+- [ ] Analyse results
+- [ ] Improve model
+
+# Source
+> Kaggle
--- mnist_digits/mnist_ditigs.py
+++ mnist_digits/mnist_ditigs.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import pandas as pd
-import seaborn as sb
+import seaborn as sns
 from random import randint
 import matplotlib.pyplot as plt
 
@@ -10,6 +10,7 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Input, Dropout, Dense, Flatten, Conv2D, MaxPooling2D
 
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
 
 import mlflow #https://mlflow.org/docs/latest/tutorials-and-examples/tutorial.html
 
@@ -59,18 +60,22 @@ x_test = x_test/255
 #Resahping to plot images
 # resheaped = np.array(x_train).reshape(-1,28,28)
 print(f"Reshaped:\n {x_train.shape}")
+
+fig2 = plt.figure()
+plt.figure(figsize=(7,7))
+for i in range(9):
+    plt.subplot(3,3, i+1)
+    image = randint(0,len(x_train))
+    plt.imshow(x_train[image],cmap='gray',interpolation='none')
+    plt.title(f"Label {y_train[image]}")
+    plt.axis('off')
+    plt.savefig(os.path.join(image_path, "samples.png"), dpi=300)
+
 if show_images:
-    plt.figure(figsize=(7,7))
-    for i in range(9):
-        plt.subplot(3,3, i+1)
-        image = randint(0,len(x_train))
-        plt.imshow(x_train[image],cmap='gray',interpolation='none')
-        plt.title(f"Label {y_train[image]}")
-        plt.axis('off')
     plt.show()
 
+#Spliting train data
 x_train, x_val, y_train, y_val = train_test_split(x_train,y_train, test_size=0.2, random_state=42)
-
 
 print(f"Train shape: {x_train.shape}")
 print(f"Train y shape: {y_train.shape}")
@@ -108,22 +113,51 @@ print(f"History: {history.history.keys()}")
 
 
 #Ploting results
+fig2 = plt.figure()
 plt.plot(history.history['accuracy'], c='blue', label='Train')
 plt.plot(history.history['val_accuracy'], c='red', label='Validation')
 plt.xlabel('Epochs')
 plt.ylabel('Accuracy')
 plt.title('Accuracy Graph')
 plt.legend()
-plt.show()
+plt.savefig(os.path.join(image_path, "accuracy_graph.png"), dpi=300)
 
+fig3 = plt.figure()
 plt.plot(history.history['loss'], c='blue', label='Train')
 plt.plot(history.history['val_loss'], c='red', label='Validation')
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
 plt.title('Loss Graph')
 plt.legend()
-plt.show()
+plt.savefig(os.path.join(image_path, "loss_graph.png"), dpi=300)
+
+if show_images:
+    plt.show()
 
 #Evaluating model
-print(f"Evaluate: {model.evaluate(x_test,y_test)}")
+score = model.evaluate(x_test,y_test)
+print(f"Evaluate: {score}")
+print(f"Loss: {score[0]}")
+print(f"Accuracy: {score[1]}")
 
+predict = model.predict(x_test)
+
+#Plotting confusion matrix to multiclass
+labels = np.sort(y_test.unique())
+conf_matrix = confusion_matrix(predict, y_test)
+cm = pd.DataFrame(conf_matrix,
+                  index= labels,
+                  columns=labels
+)
+
+fig4 = plt.figure()
+sns.heatmap(cm, annot=True)
+plt.title("Confusion Matrix")
+plt.xlabel("Predicted Values")
+plt.ylabel("Actual Values")
+plt.savefig(os.path.join(image_path,"confusion_matrix.png"),dpi=300)
+if show_images:
+    plt.show()
+
+#Save model
+model.save("mnist_digits_model.h5")
